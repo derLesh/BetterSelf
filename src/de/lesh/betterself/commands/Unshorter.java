ff25a77f770033f271ee93185a7711547945bb1e
Lesh <gamelesh@gmail.com>
2017-07-08 22:54:56 +0200

    .

--- src/de/lesh/betterself/commands/Unshorter.java
+++ src/de/lesh/betterself/commands/Unshorter.java
@@ -1,5 +1,11 @@
 package de.lesh.betterself.commands;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.concurrent.TimeUnit;
 
 import de.lesh.betterself.util.Config;
@@ -10,7 +16,7 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class Unshorter extends ListenerAdapter {
 	
 	public static Config CONFIG = new Config();
-
+	public static String output;
 	
 	public void onMessageReceived(MessageReceivedEvent e){
 		Message msg = e.getMessage();
@@ -20,7 +26,22 @@ public class Unshorter extends ListenerAdapter {
 			String unshorting = "";
 			unshorting = split[1];
 			String unshortener = "https://unshorten.me/s/" + unshorting;
-			e.getChannel().sendMessage("Unshorted link: " + unshortener).queue();
+			
+			try {
+				URL shorten = new URL(unshortener);
+				URLConnection connect = shorten.openConnection();
+				BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+				String result;
+				while((result = in.readLine()) != null){
+					output = result;
+				}
+				
+			} catch (MalformedURLException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			e.getChannel().sendMessage("Unshorted link: " + output).queue();
 			
 			System.out.println("Succesful shorted");
 			e.getMessage().delete().queueAfter(1, TimeUnit.MILLISECONDS);
