3a4d81a6005311b5ecd55bb9699bd423b1346ab1
Lesh <gamelesh@gmail.com>
2017-07-08 23:14:19 +0200

    Adding BAN Command

--- src/de/lesh/betterself/commands/Unshorter.java
+++ src/de/lesh/betterself/commands/Unshorter.java
@@ -3,6 +3,7 @@ package de.lesh.betterself.commands;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -21,7 +22,7 @@ public class Unshorter extends ListenerAdapter {
 	public void onMessageReceived(MessageReceivedEvent e){
 		Message msg = e.getMessage();
 		
-		if (msg.getRawContent().startsWith("+unshort")) {
+		if (msg.getRawContent().startsWith(CONFIG.prefix + "unshort")) {
 			String[] split = e.getMessage().getRawContent().split("\\s+", 2);
 			String unshorting = "";
 			unshorting = split[1];
@@ -29,21 +30,22 @@ public class Unshorter extends ListenerAdapter {
 			
 			try {
 				URL shorten = new URL(unshortener);
-				URLConnection connect = shorten.openConnection();
+				HttpURLConnection connect = (HttpURLConnection)shorten.openConnection();
+				connect.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+				connect.setRequestMethod("GET");
 				BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
 				String result;
 				while((result = in.readLine()) != null){
 					output = result;
 				}
-				
 			} catch (MalformedURLException e1) {
 				e1.printStackTrace();
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-			e.getChannel().sendMessage("Unshorted link: " + output).queue();
+			e.getChannel().sendMessage("[UNSHORTENER] >> Original link: " + output).queue();
 			
-			System.out.println("Succesful shorted");
+			System.out.println("[SUCCESFUL] >> UNSHORTED - Original: " + unshorting + " - Unshorted: " + output);
 			e.getMessage().delete().queueAfter(1, TimeUnit.MILLISECONDS);
 		}	
 	}
---
+++ src/de/lesh/betterself/commands/admin/Ban.java
@@ -0,0 +1,5 @@
+package de.lesh.betterself.commands.admin;
+
+public class Ban {
+
+}
